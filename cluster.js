4997928b0f94d11520815b1b66832040a1056154
Madhu Kumar D S <madhudskumar@live.in>
2018-03-03 16:52:52 +0530

    u:clusters to handle multiple treads

--- cluster.js
+++ cluster.js
@@ -1,14 +1,19 @@
 const cluster = require('cluster');
+const numCpus = require('os').cpus().length;
 
-if(cluster.isMaster) {
+if (cluster.isMaster) {
     console.log(`master on ${process.pid}`);
+    
+    let i = 0;
+    while(i++ < numCpus) {
+        cluster.fork();
+    }
 
-    cluster.fork();
-
-    cluster.on('exit', function(_, _, _) {
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`);
         console.log('reiniting...');
         cluster.fork();
-    })
+    });
 } else {
     console.log(`child on ${process.pid}`);
     require('./app');
